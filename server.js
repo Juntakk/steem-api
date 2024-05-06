071d8c428778b0aa20273b60ef971671c10fb414
Juntakk <nick.habashi@gmail.com>
2024-05-05 21:26:38 -0400

    First

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ const cors = require("cors");
 const gameRoutes = require("./routes/game.routes");
 const userRoutes = require("./routes/user.routes");
 const path = require("path");
+
 const MONGO_URI =
   "mongodb+srv://juntakk:Password123!@games.oxleerl.mongodb.net/?retryWrites=true&w=majority&appName=games";
+
 const app = express();
 
 app.use(express.json());
+
 app.use(
   cors({
-    origin: ["http://localhost:5000", "https://steem-frontend.onrender.com"],
+    origin: ["http://localhost:3000", "https://steem-frontend.onrender.com"],
   })
 );
+
+// app.use(cors());
+
 mongoose.connect(MONGO_URI);
 const PORT = process.env.PORT || 5000;
 
